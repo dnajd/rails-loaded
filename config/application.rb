a81cccd584185a12e2f23948bbc56463015742d1
Don Najd <dnajd7@gmail.com>
2015-04-29 00:44:10 -0700

    disable stylesheet and javascript generation; got bitters working; added reset

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module RailsLoaded
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # disabling these
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
   end
 end
