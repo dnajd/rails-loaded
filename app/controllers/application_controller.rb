f133aa315bf27656eb4bc0d80190900431d998fe
Don Najd <dnajd7@gmail.com>
2015-06-09 06:45:19 -0700

    guid generation and controller / action on body class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :create_guid
+
+  def create_guid
+    # only if it doesn't exist already?
+    if session[:guid].blank?
+      session[:guid] = SecureRandom.urlsafe_base64
+    end
+  end
+
   def append_info_to_payload(payload)
     super
     payload[:host] = request.host
