3d32ccf4b475e36bb9478987189c55f211dbc028
Don Najd <dnajd7@gmail.com>
2015-06-10 08:42:58 -0700

    keep original rails log files; this solved it

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module RailsLoaded
     ##############################
     # LOGRAGE
     ##############################
+    config.lograge.keep_original_rails_log = true
+    config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
     config.lograge.enabled = true
     config.lograge.custom_options = lambda do |event|
 
