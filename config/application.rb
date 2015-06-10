b3832053fee5db028ca2d89ab4f571c862eab729
Don Najd <dnajd7@gmail.com>
2015-06-10 08:47:44 -0700

    log rage logfile

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module RailsLoaded
     # LOGRAGE
     ##############################
     config.lograge.keep_original_rails_log = true
-    config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
+    config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lr_#{Rails.env}.log"
     config.lograge.enabled = true
     config.lograge.custom_options = lambda do |event|
 
